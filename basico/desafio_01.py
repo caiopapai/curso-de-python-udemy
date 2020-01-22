a46a6603d7821eb4820356c1a4ccc2ba73697ea9
Caio Augusto Papai <cpapai@uolinc.com>
2020-01-22 18:40:44 -0300

    tipos basicos mais desafio

---
+++ basico/desafio_01.py
@@ -0,0 +1,4 @@
+salario = 4000.00
+despesas = 3586.69
+percentual = despesas / salario * 100
+print('O seu salário está {} comprometido'.format(percentual))
---
+++ basico/tipos_basicos.py
@@ -0,0 +1,10 @@
+a = True
+b = False
+
+nulo = None
+
+print(a)
+print(b)
+print([0, 1,2])
+print({'nome':'teste', 'idade':40})
+print(nulo)
